c3b0ff96312235a15c5866a4880df08472e99ec2
hnolasco <hnolasco>
2020-07-29 10:46:42 -0500

    ---se deja ajustado el acceso mediante loclhost

--- src/app/shared/global.ts
+++ src/app/shared/global.ts
@@ -1,6 +1,6 @@
 export const GlobalVariable = Object.freeze({
-    //BASE_API_URL: 'https://localhost:44354',
+    BASE_API_URL: 'http://localhost:44354',
     // BASE_API_URL: 'http://localhost:8090/'
-   BASE_API_URL: 'http://10.10.50.175:8080'
+   //BASE_API_URL: 'http://10.10.50.175:8080'
 
 });
